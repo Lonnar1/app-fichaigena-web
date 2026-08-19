34bf3b9dd0ad8607516090edb9fa1f11a36ba6f6
Lonnar <lonnar321@gmail.com>
2026-08-19 08:18:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1288,6 +1288,7 @@ function getIconeTipo(tipo) {
   if (t.includes("gelo")) return "❄️";
   if (t.includes("agua")) return "💧";
   if (t.includes("raio")) return "⚡";
+  if (t.includes("cura")) return "🩹";
   if (t.includes("trovej")) return "🌩️";
   if (t.includes("necrot")) return "💀";
   if (t.includes("radiante")) return "✨";
@@ -1303,10 +1304,8 @@ function getIconeTipo(tipo) {
   if (t.includes("espirit")) return "🌓";
   if (t.includes("vento")) return "🍃";
   if (t.includes("madeira")) return "🌳";
- if (t.includes("terra")) return "🌍";
+  if (t.includes("terra")) return "🌍";
   if (t.includes("metal")) return "⚙️";
-
-  return "🔮";
 
   return "🔮";
 }
@@ -1764,6 +1763,7 @@ function editarPoder(index) {
   <option value="fogo" ${normalizarTipo(poder.tipo) === "fogo" ? "selected" : ""}>🔥 Fogo</option>
   <option value="gelo" ${normalizarTipo(poder.tipo) === "gelo" ? "selected" : ""}>❄️ Gelo</option>
   <option value="raio" ${normalizarTipo(poder.tipo) === "raio" ? "selected" : ""}>⚡ Raio</option>
+  <option value="cura" ${normalizarTipo(poder.tipo) === "cura" ? "selected" : ""}>🩹 Cura</option>
   <option value="trovejante" ${normalizarTipo(poder.tipo) === "trovejante" ? "selected" : ""}>🌩️ Trovejante</option>
   <option value="necrotico" ${normalizarTipo(poder.tipo) === "necrotico" ? "selected" : ""}>💀 Necrótico</option>
   <option value="radiante" ${normalizarTipo(poder.tipo) === "radiante" ? "selected" : ""}>✨ Radiante</option>
